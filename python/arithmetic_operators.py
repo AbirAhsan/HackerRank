7fe2574f9195cbab397209752b0a6794d2ac0ff6
Abir Ahsan <abirahsan122@gmail.com>
2022-09-13 21:09:54 +0600

    add arithmetic operators

---
+++ python/arithmetic_operators.py
@@ -0,0 +1,46 @@
+# Arithmetic Operators
+
+# Task
+# The provided code stub reads two integers from STDIN a and b. Add code to print three lines where:
+# 1. The first line contains the sum of the two numbers.
+# 2. The second line contains the difference of the two numbers(first - second).
+# 3. The third line contains the product of the two numbers
+# Hi
+
+# Example
+# a = 3
+# b = 5
+
+# Print the following:
+# 8
+# -2
+# 15
+
+# Input Format
+# The first line contains the first integer, a.
+# The second line contains the second integer, b.
+
+# Output Format
+# Print the three lines as explained above.
+
+
+# Sample Input :
+# 3
+# 2
+
+# Sample Output :
+# 5
+# 1
+# 6
+
+
+# if __name__ == '__main__':
+a = int(input())
+b = int(input())
+
+sumation = a + b
+dif = a - b
+multiplication = a*b
+print(sumation)
+print(dif)
+print(multiplication)
